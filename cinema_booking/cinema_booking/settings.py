21449815b862f930136e46cb1cb49ff76bf06e55
Kirill Andreev <Hizenberg228@,ail.ru>
2020-12-13 18:49:49 +0300

    Add djoser

--- cinema_booking/cinema_booking/settings.py
+++ cinema_booking/cinema_booking/settings.py
@@ -27,7 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +125,7 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
 }
--- cinema_booking/cinema_booking/urls.py
+++ cinema_booking/cinema_booking/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+
 
 ]
